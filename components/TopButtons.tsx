335954925d03c0dfefe6024db134c5f24b87cbf1
Abhishek-B-R <byteblaster1468@gmail.com>
2025-07-25 18:53:02 +0530

    fixed top navbar's z index issue

--- components/TopButtons.tsx
+++ components/TopButtons.tsx
@@ -7,13 +7,15 @@ import { PenTool } from "lucide-react"
 
 export default function TopButtons() {
   return (
-    <div className="fixed top-4 xl:right-[400px] lg:right-80 md:right-40 sm:right-20 right-10 z-50 flex space-x-3 mt-2">
-      <ThemeToggle />
-      <Link href="/guestbook">
-        <Button size="sm" className="bg-blue-600 cursor-pointer hover:bg-blue-800 text-white">
-          <PenTool /> Sign my Guestbook
-        </Button>
-      </Link>
+    <div className="fixed top-0 z-100 bg-background w-full h-18">
+      <div className="fixed top-4 xl:right-[350px] lg:right-80 md:right-40 sm:right-20 right-10 z-100 flex space-x-3 mt-2">
+        <ThemeToggle />
+        <Link href="/guestbook">
+          <Button size="sm" className="bg-blue-600 cursor-pointer hover:bg-blue-800 text-white">
+            <PenTool /> Sign my Guestbook
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }
